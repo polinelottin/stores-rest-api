b4844cc19fe6a7970889a7540133eea17475a49f
Poline Lottin <lottinp@gmail.com>
2018-04-29 17:20:10 -0300

    fixed not importing db when running the app

--- app.py
+++ app.py
@@ -15,10 +15,6 @@ app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
